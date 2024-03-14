605743d1fc2ba4877f593ab8a8ad46d931962adf
Luisa Herrera <103288010+Luisaherrera01@users.noreply.github.com>
2024-03-14 10:17:26 -0500

    Update ICloneable.cs
    se quita comentario

--- Prototype/ICloneable.cs
+++ Prototype/ICloneable.cs
@@ -4,7 +4,6 @@ namespace Prototype
 {
     public interface ICloneable
     {
-        //Object es la clase base de todos los clases, aqui se devuelve un tipo de datos. 
         object Clone();
     }
 }
